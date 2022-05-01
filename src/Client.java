a235e1689c03aeea20316fecd283428f47681720
LyalinDima <lyalindim@gmail.com>
2022-05-01 16:50:20 +0500

    First commit

---
+++ src/Client.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+    public static void main(String[] args) {
+        try (Socket clientSocket = new Socket(Server.SERVER_HOST, Server.SERVER_PORT);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Socket Client");
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
